087e4010bc274edd0593b2448855e9b40e7c109f
Frans Saukko <frans@equilibrium.co>
2019-09-16 10:45:27 +0300

    Add license for JS files

--- js/identity-provider-interface.js
+++ js/identity-provider-interface.js
@@ -1,3 +1,6 @@
+//		MIT © 2016-2018 Protocol Labs Inc., 2016-2019 Haja Networks Oy
+//		https://github.com/orbitdb/ipfs-log
+
 'use strict'
 
 class IdentityProvider {
--- js/keystore.js
+++ js/keystore.js
@@ -1,3 +1,6 @@
+//		MIT © 2016-2018 Protocol Labs Inc., 2016-2019 Haja Networks Oy
+//		https://github.com/orbitdb/ipfs-log
+
 'use strict'
 
 const fs = (typeof window === 'object' || typeof self === 'object') ? null : eval('require("fs")') // eslint-disable-line
